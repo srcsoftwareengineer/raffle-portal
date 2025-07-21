677749959e35a63141aaa3bc4377d4fff8984ed0
masterdev <masterdev@imhotepxpythondev>
2025-07-21 01:26:03 -0300

    Included attribute status and class docstring

--- core/models/raffles.py
+++ core/models/raffles.py
@@ -3,12 +3,17 @@ from django.contrib.auth.models import User
 
 
 class Raffle(models.Model):
+    """
+    @summary: Create relational entity 'raffles'
+    """
+
     created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
     description = models.TextField()
     total_tickets = models.PositiveIntegerField()
     ticket_price = models.DecimalField(max_digits=10, decimal_places=2)
     draw_date = models.DateField()
+    status = models.CharField(max_length=10, default="draft")
 
     def __str__(self):
         return f"{self.title} - {self.total_tickets} tickets"
