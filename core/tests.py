2475d2c1520e64612e728bb848204cd0a166bf1f
masterdev <masterdev@imhotepxpythondev>
2025-07-21 01:32:37 -0300

    Added test_purchase_view_accessible method for purchase interface route

--- core/tests.py
+++ core/tests.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class Tests(TestCase):
-    def test_dummy(self):
+
+    def test_dummy_1(self):
         a = 1
         b = 1
-        self.assertEqual(a, b)
+        self.assertEquals(a, b)
+
+    def test_purchase_view_accessible(self):
+        response = self.client.get(reverse("core:purchase_tickets", args=[1]))
+        self.assertIn(response.status_code, [200, 302])
