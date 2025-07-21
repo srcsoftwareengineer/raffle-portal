1fc079e9b856a4702a07f3eb430f8d6e0adbbce5
masterdev <masterdev@imhotepxpythondev>
2025-07-21 01:29:52 -0300

    Code review to process the raffle available tickets and code reformat

--- core/app_forms/ticket_form.py
+++ core/app_forms/ticket_form.py
@@ -16,8 +16,34 @@ class TicketPurchaseForm(forms.Form):
         label="Select tickets",
     )
 
+    def _get_available_tickets(self, raffle: object):
+        def fill_tickets_choices():
+            self.fields["tickets"].choices = [
+                (t.id, f"Nº {t.number}") for t in available_numbers
+            ]
+
+        def create_tickets():
+            assert raffle.total_tickets > 0
+            idx: int = raffle.total_tickets
+            while idx > 0:
+                Ticket.objects.create(
+                    raffle=raffle, number=idx, buyer_id=None, is_reserved=False
+                )
+                idx -= 1
+            fill_tickets_choices()
+
+        available_numbers = Ticket.objects.filter(raffle=raffle.id, buyer__isnull=True)
+        if available_numbers.exists():
+            fill_tickets_choices()
+        else:
+            create_tickets()
+
+    def get_available_tickets(self, raffle: object) -> list:
+        self._get_available_tickets(raffle)
+        return self.fields["tickets"].choices
+
     def __init__(self, *args, raffle=None, **kwargs):
         super().__init__(*args, **kwargs)
         if raffle:
-            available = Ticket.objects.filter(raffle=raffle, buyer__isnull=True)
-            self.fields["tickets"].choices = [(t.id, f"#{t.number}") for t in available]
+            self.get_available_tickets(raffle)
+        return
