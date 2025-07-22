c0c35f8ba1fc4684bd043b5b36dd3d5098a8595a
masterdev <masterdev@imhotepxpythondev>
2025-07-22 04:19:05 -0300

    New attribute 'winner' and remove unused import

--- core/models/raffles.py
+++ core/models/raffles.py
@@ -2,7 +2,6 @@
 
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
 
 
 class Raffle(models.Model):
@@ -21,24 +20,9 @@ class Raffle(models.Model):
     create_date = models.DateField(auto_now_add=True)
     drawn_at = models.DateTimeField(null=True, blank=True)  # Hora real do sorteio
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    winner = models.ForeignKey(
+        "core.Ticket", null=True, blank=True, on_delete=models.SET_NULL
+    )
 
     def __str__(self):
         return f"{self.title} ({self.status})"
-
-
-# class Raffle(models.Model):
-#     """
-#     @summary: Create relational entity 'raffles'
-#     """
-#
-#     created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-#     title = models.CharField(max_length=200)
-#     description = models.TextField()
-#     total_tickets = models.PositiveIntegerField()
-#     ticket_price = models.DecimalField(max_digits=10, decimal_places=2)
-#     draw_date = models.DateField()
-#     # drawn_at = models.DateTimeField(null=True, blank=True)
-#     status = models.CharField(max_length=10, default="draft")
-#
-#     def __str__(self):
-#         return f"{self.title} - {self.total_tickets} tickets"
