0b3587540f6b42f9282f9682196c1cdd48b5f040
masterdev <masterdev@imhotepxpythondev>
2025-07-24 01:16:53 -0300

    Inclusion of Winner notification management

--- core/admin.py
+++ core/admin.py
@@ -4,5 +4,13 @@ from core.models.winners import WinnerNotification
 
 @admin.register(WinnerNotification)
 class WinnerNotificationAdmin(admin.ModelAdmin):
-    list_display = ("raffle", "winner", "timestamp", "wapp_sent")
+    list_display = ("raffle", "winner", "wapp_sent")
     list_filter = ("wapp_sent",)
+
+
+# DEVTOOLS
+# from django.contrib.auth.models import Group, User
+#
+# group, created = Group.objects.get_or_create(name="super-admin")
+# admin_user = User.objects.get(username="admin")
+# admin_user.groups.add(group)
